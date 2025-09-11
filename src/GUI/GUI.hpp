7f871c7d74137ff37baf7334922fd66fa5195fe1
kai552 <mystical4339@gmail.com>
2025-09-11 21:12:26 +0900

    Update GUI.hpp

--- src/GUI/GUI.hpp
+++ src/GUI/GUI.hpp
@@ -6,12 +6,23 @@ using namespace geode::prelude;
 class UI : public geode::Popup<> {
 protected:
     CCMenu* m_bottomButtonsMenu;
+    CCScrollView* m_scrollView;
+    CCMenu* m_contentMenu;
+    std::string m_currentCategory;
 
     bool setup() override;
+    void onExit() override;
 
     void addDefaultUICategoryButton(const std::string& category);
     void addLabel(const std::string& category);
     void onCategoryButtonPressed(CCObject* sender);
+    void onCheckboxToggled(CCObject* sender);
+    void setupCategoryContent(const std::string& category);
+    void clearContent();
+    
+    void saveCheckboxState(const std::string& key, bool value);
+    bool loadCheckboxState(const std::string& key, bool defaultValue = false);
+    void saveAllStates();
 public:
     static UI* create() {
         auto ret = new UI();
