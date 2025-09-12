9bb1c52b290e3c85ce5dc1052a74f10be4459169
kai552 <mystical4339@gmail.com>
2025-09-12 15:20:21 +0900

    Update KeyBindHooks.cpp

--- src/Client/KeyBindHooks.cpp
+++ src/Client/KeyBindHooks.cpp
@@ -4,7 +4,7 @@
 class $modify(AmethystUIHook, cocos2d::CCKeyboardDispatcher) {
     bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
         if (key == cocos2d::enumKeyCodes::KEY_Tab && isKeyDown) {
-            UI::create()->create();
+            UI::create()->show();
         }
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
     }
