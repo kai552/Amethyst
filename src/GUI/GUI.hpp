6bf5bf5539b2e1bcd2bc5618ea3a02463771f72d
kai552 <mystical4339@gmail.com>
2025-09-12 15:00:10 +0900

    I hate you all

--- src/GUI/GUI.hpp
+++ src/GUI/GUI.hpp
@@ -3,54 +3,33 @@
 #include <Geode/Geode.hpp>
 #include <unordered_map>
 #include <string>
+#include <vector>
+#include "../../Client.hpp"
 using namespace geode::prelude;
 
-class SettingsManager {
-private:
-    static SettingsManager* instance;
-    std::unordered_map<std::string, bool> settings;
+enum class UIElementType {
+    Checkbox,
+    Button,
+    Float
+};
+
+struct UIElement {
+    std::string name;
+    UIElementType type;
+    std::string key;
+};
+
+class FloatConfigPopup : public geode::Popup<> {
+protected:
+    std::string m_settingKey;
+    CCTextInputNode* m_input;
     
-    SettingsManager() = default;
-
+    bool setup(const std::string& settingKey) override;
+    void onSave(CCObject* sender);
+    void onCancel(CCObject* sender);
+    
 public:
-    static SettingsManager* get();
-    bool getIfEnabled(const std::string& settingName);
-    void setEnabled(const std::string& settingName, bool enabled);
-    void toggle(const std::string& settingName);
-    void saveAll();
-};
-
-class SolidWave {
-public:
-    static SolidWave* get();
-    bool getIfEnabled();
-    void setEnabled(bool enabled);
-    void toggle();
-};
-
-class SpeedHack {
-public:
-    static SpeedHack* get();
-    bool getIfEnabled();
-    void setEnabled(bool enabled);
-    void toggle();
-};
-
-class Noclip {
-public:
-    static Noclip* get();
-    bool getIfEnabled();
-    void setEnabled(bool enabled);
-    void toggle();
-};
-
-enum class Category {
-    Player,
-    Creator,
-    Misc,
-    Cosmetic,
-    Settings,
-    Credits
+    static FloatConfigPopup* create(const std::string& settingKey);
 };
 
 class UI : public geode::Popup<> {
@@ -64,16 +43,18 @@ protected:
     void onExit() override;
 
     void addDefaultUICategoryButton(const std::string& category);
-    void addLabel(const std::string& category);
     void onCategoryButtonPressed(CCObject* sender);
     void onCheckboxToggled(CCObject* sender);
+    void onButtonPressed(CCObject* sender);
+    void onFloatButtonPressed(CCObject* sender);
     void setupCategoryContent(const std::string& category);
     void clearContent();
     
     void saveCheckboxState(const std::string& key, bool value);
     bool loadCheckboxState(const std::string& key, bool defaultValue = false);
     void saveAllStates();
-    std::vector<std::string> getCategoryFields(const std::string& categoryStr);
+    std::vector<UIElement> getCategoryElements(const std::string& categoryStr);
+    
 public:
     static UI* create() {
         auto ret = new UI();
